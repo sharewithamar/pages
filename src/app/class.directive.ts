3fd533264767dd2288955e1b45b11b418b2b6dbb
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2020-02-25 15:20:29 +0530

    using Input and setter to pass and setter to set the value for the instance.

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,10 +1,19 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, Input } from "@angular/core";
 
 @Directive({
   selector: "[appClass]"
 })
 export class ClassDirective {
+  //@Input() backgroundColor: string;
+
   constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = "red";
+    // NEVER DO THIS
+    /*     setTimeout(() => {
+      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    }, 50); */
+  }
+
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
   }
 }
